ca8ec3dc0bd1ab75d1998441d46d388cbf0f4afd
Krzysztof Weimann <104395816+Krzysztofweimann02@users.noreply.github.com>
2023-11-22 13:28:40 +0100

    fixed layout for small devices

--- lib/SudokuView.dart
+++ lib/SudokuView.dart
@@ -24,86 +24,91 @@ class _SudokuViewState extends State<SudokuView> {
           backgroundColor: darkMode ? Colors.red[800] : Colors.red[200],
           title: Text("Sudoku Solver",
               style: TextStyle(color: darkMode ? Colors.white : Colors.black))),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SudokuBoardWidget(),
-            Padding(
-              padding: const EdgeInsets.all(5),
-              child: Center(
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Text("Tryb ciemny: ",
-                            style: TextStyle(
-                                color: darkMode ? Colors.white : Colors.black)),
-                        Switch(
-                          value: darkMode,
-                          onChanged: (bool value) {
-                            if (enableSounds) {
-                              player.setVolume(volume);
-                              player.play(AssetSource("click.mp3"));
-                            }
-                            setState(() {
-                              darkMode = value;
-                            });
-                          },
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Text("Dźwiękowe: ",
-                            style: TextStyle(
-                                color: darkMode ? Colors.white : Colors.black)),
-                        Switch(
-                          value: enableSounds,
-                          onChanged: (bool value) {
-                            if (enableSounds) {
-                              player.setVolume(volume);
-                              player.play(AssetSource("click.mp3"));
-                            }
-                            setState(() {
-                              enableSounds = value;
-                            });
-                          },
-                        ),
-                        if (enableSounds)
-                          Padding(
-                              padding: EdgeInsets.all(5),
-                              child: Row(
-                                children: [
-                                  Text("Głośność: ",
-                                      style: TextStyle(
-                                          color: darkMode
-                                              ? Colors.white
-                                              : Colors.black)),
-                                  Slider(
-                                    label: volume.round().toString(),
-                                    value: volume,
-                                    onChanged: (double value) {
-                                      setState(() {
-                                        volume = value;
-                                        player.setVolume(volume);
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SudokuBoardWidget(),
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: Center(
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text("Tryb ciemny: ",
+                              style: TextStyle(
+                                  color:
+                                      darkMode ? Colors.white : Colors.black)),
+                          Switch(
+                            value: darkMode,
+                            onChanged: (bool value) {
+                              if (enableSounds) {
+                                player.setVolume(volume);
+                                player.play(AssetSource("click.mp3"));
+                              }
+                              setState(() {
+                                darkMode = value;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text("Dźwiękowe: ",
+                              style: TextStyle(
+                                  color:
+                                      darkMode ? Colors.white : Colors.black)),
+                          Switch(
+                            value: enableSounds,
+                            onChanged: (bool value) {
+                              if (enableSounds) {
+                                player.setVolume(volume);
+                                player.play(AssetSource("click.mp3"));
+                              }
+                              setState(() {
+                                enableSounds = value;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                      if (enableSounds)
+                        Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Row(
+                            children: [
+                              Text("Głośność: ",
+                                  style: TextStyle(
+                                      color: darkMode
+                                          ? Colors.white
+                                          : Colors.black)),
+                              Slider(
+                                label: volume.round().toString(),
+                                value: volume,
+                                onChanged: (double value) {
+                                  setState(() {
+                                    volume = value;
+                                    player.setVolume(volume);
 
-                                        if (volume == 0) {
-                                          enableSounds = false;
-                                          volume = 1;
-                                        }
-                                      });
-                                    },
-                                  )
-                                ],
-                              )),
-                      ],
-                    ),
-                  ],
+                                    if (volume == 0) {
+                                      enableSounds = false;
+                                      volume = 1;
+                                    }
+                                  });
+                                },
+                              )
+                            ],
+                          ),
+                        ),
+                    ],
+                  ),
                 ),
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
